bbb69dd4f3cd1fcf991fd44961311e99b60ef292
sheidiaodenaihuangbao <3158898150@qq.com>
2024-10-06 00:04:40 +0800

    10_5

--- src/Swap.java
+++ src/Swap.java
@@ -2,7 +2,7 @@ public class Swap {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int temp = 1;
+        int temp;
         temp = a;
         a = b;
         b = temp;
